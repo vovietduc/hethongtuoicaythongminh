31ffa34a624ea0f1126bb02ca1e6eabafae3287b
vovietduc <vovietduc2006@gmail.com>
2026-07-04 01:15:53 +0700

    Update WiFi and Blynk configuration details

---
+++ config.h
@@ -0,0 +1,18 @@
+// config.h
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// WiFi
+#define WIFI_SSID     "Duc Huy"
+#define WIFI_PASSWORD "29082012"
+
+// Blynk
+#define BLYNK_TEMPLATE_ID   "TMPL6VB_DZigB"
+#define BLYNK_DEVICE_NAME   "ESP321"
+#define BLYNK_AUTH_TOKEN    "yE_YW59wlLj1SqMiC9a6coWzAy-EimTk"
+
+// ThingSpeak
+#define THINGSPEAK_API_KEY  "
+NYKUBQU61JY56GZ9"
+
+#endif
